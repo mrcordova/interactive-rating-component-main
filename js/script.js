126c7b810036ce6d34ea0c5fe9c07dad4a3ea094
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-29 14:36:08 -0700

    add code for submit btn

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const scoreRadioChoices = document.querySelectorAll('input[name="ratings"]');
+const submitBtn = document.querySelector('.submit-btn');
+
+let currentScoreVal = document.querySelector('input[type="radio"]:checked').value;
+
+
+
+const updateScoreVal = function (e) {
+    currentScoreVal = e.currentTarget.value;
+    
+}
+
+for (const scoreRadioChoice of scoreRadioChoices) {
+    scoreRadioChoice.addEventListener('click', updateScoreVal);
+}
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const scorePara = document.querySelector('.thank-you-score');
+    const thankYouCard = document.querySelector('.thank-you-card');
+    const ratingsCard = document.querySelector('.ratings-card');
+
+    scorePara.textContent = `You selected ${currentScoreVal} out of 5`;
+    thankYouCard.classList.toggle('hide');
+    ratingsCard.classList.toggle('hide');
+})
